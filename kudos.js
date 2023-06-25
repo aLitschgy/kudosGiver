0690f349eb8c79e120eb739bf35aabf7c677cda6
Antonin <an.litschgy@orange.fr>
2023-06-25 12:14:53 +0200

    feat: Corps de l'extension

---
+++ kudos.js
@@ -0,0 +1,22 @@
+// Amélioration future : ne donner des kudos que si la personne a été sélectionnée dans une liste
+
+// Dans l'interface :
+//      Propositions : tout le monde / tout le monde sauf / uniquemenent ces personnes / personne
+//      Donner des kudos : Au chargement de la page / En appuyant sur le bouton
+//      Nombre d'activités à charger : (int) /!\ Peut ralentir le chargement de la page
+
+boutons = document.querySelectorAll('[title="Envoyer des kudos"]')
+
+function giveKudos(boutons, i = 0) {
+    // Il faut mettre un timeout sinon on se fait bloquer par l'api pour trop nombreuses requêtes
+    if (i < boutons.length) {
+        setTimeout(function () {
+            giveKudos(boutons, i + 1)
+        }, 1000)
+        boutons[i].click()
+    }
+
+}
+
+document.body.style.border = '5px solid red';
+
---
+++ popup/kudos.js
@@ -0,0 +1,4 @@
+buttonSend = document.querySelector("#send");
+buttonSend.addEventListener("click", (e) => {
+    console.log("button clicked");
+});
